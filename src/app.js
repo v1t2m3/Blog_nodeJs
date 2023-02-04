36ddadb7f277a5609fbe2e6e5b9fabae19b7cd5a
v1t2m3 <nguyenvantamdn@gmail.com>
2023-02-04 15:46:37 +0700

    update

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+//static file
+app.use(express.static(path.join(__dirname, "resources/public")));
+
+//http logger
+app.use(morgan("combined"));
+//template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
